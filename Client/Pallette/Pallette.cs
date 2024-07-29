5d41a4953aed5eae9d97ee52e7b37a4e68220c04
Manuel Rodas Delgado <ketsalmrd@gmail.com>
2024-07-29 17:42:46 +0200

    Initial

---
+++ Client/Pallette/Pallette.cs
@@ -0,0 +1,11 @@
+namespace application.Pallette;
+
+public static class Pallette
+{
+    public static string VIOLET_DARK = "#741444";
+    public static string GREY = "#2B2327 ";
+    public static string OFF_WHITE = "#F5F5F5 ";
+    public static string PINK = "#DB0371";
+    public static string BLACK = "#080808";
+
+}
